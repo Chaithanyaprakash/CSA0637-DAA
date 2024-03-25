d5c10ade33213e9c70a800644131cb7dce353b7a
Chaithanyaprakash <129160651+Chaithanyaprakash@users.noreply.github.com>
2024-03-25 12:26:36 +0530

    Update 1.Print fibonacci using recursion.cpp

--- 1.Print fibonacci using recursion.cpp
+++ 1.Print fibonacci using recursion.cpp
@@ -1,33 +1,34 @@
 #include <stdio.h>
 
-int power(int base, int exponent) {
-    if (exponent == 0)
-        return 1;
-    return base * power(base, exponent - 1);
+// Function to calculate Fibonacci number using recursion
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-int countDigits(int num) {
-    if (num == 0)
-        return 0;
-    return 1 + countDigits(num / 10);
-}
-
-int isArmstrong(int num, int originalNum) {
-    if (num == 0)
-        return 0;
-    return power(num % 10, countDigits(originalNum)) + isArmstrong(num / 10, originalNum);
+// Function to print Fibonacci series up to a given number of terms
+void printFibonacciSeries(int terms) {
+    int i;
+    printf("Fibonacci Series: ");
+    for (i = 0; i < terms; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
 }
 
 int main() {
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    if (isArmstrong(num, num) == num)
-        printf("%d is an Armstrong number.\n", num);
-    else
-        printf("%d is not an Armstrong number.\n", num);
+    int terms;
+    printf("Enter the number of terms: ");
+    scanf("%d", &terms);
+    
+    if (terms <= 0) {
+        printf("Invalid input. Please enter a positive number.\n");
+        return 1; // Exit with error code
+    }
+    
+    printFibonacciSeries(terms);
 
     return 0;
 }
-
